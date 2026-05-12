5b2fe8fb9f7d13f3a9666b28c3308eca04c8e06b
student.jo <student.jo@europris.no>
2026-05-12 17:04:51 +0200

    Lagt til ikon for browser samt navn for applikasjonen

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Bachelorgruppe B26 - IKT37",
+  title: "BildeGenereringsVerktøyEP",
   description: "Laget av Studenter",
 };
 
